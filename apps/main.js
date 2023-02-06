b7a9b00de757a2450a880c3f985ba95053d2ae4b
win <akarawin@daytch.co.th>
2023-02-06 22:28:25 +0700

    feat: create park function

--- apps/main.js
+++ apps/main.js
@@ -1,5 +1,7 @@
 class Car {
   constructor(registrationNumber, color) {
+    this.registrationNumber = registrationNumber
+    this.color = color
     this.slot = null
   }
 }
@@ -7,8 +9,24 @@ class Car {
 class ParkingLot {
   constructor(slots) {
     this.slots = Array(slots).fill(null)
+    this.cars = []
+  }
+
+  park(car) {
+    for (let i = 0; i < this.slots.length; i++) {
+      if (this.slots[i] === null) {
+        this.slots[i] = car
+        car.slot = i + 1
+        this.cars.push(car)
+        console.log(`Allocated slot number: ${car.slot}`)
+        return
+      }
+    }
+    console.log('Sorry, parking lot is full')
   }
 }
 
 const parkingLot = new ParkingLot(6)
+console.log('parkingLot: ', parkingLot)
+parkingLot.park(new Car('KA-01-HH-1234', 'White'))
 console.log('parkingLot: ', parkingLot)
